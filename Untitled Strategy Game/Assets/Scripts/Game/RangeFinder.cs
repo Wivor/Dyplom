75cf7c4a4372a06d1866851e11a932bbbc269063
Wivor <wiktor756@gmail.com>
2019-11-18 16:33:53 +0100

    Implemented rangeFinder

---
+++ Untitled Strategy Game/Assets/Scripts/Game/RangeFinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeFinder
+{
+    public List<Node> inRange = new List<Node>();
+
+    public void FindNodesInRange(Node node, int range, int iteration = 1)
+    {
+        Debug.Log(range);
+        if (range != 0)
+        {
+            foreach (Node neighbour in node.neighbours)
+            {
+                if(neighbour.distance == 0 || neighbour.distance < iteration)
+                {
+                    neighbour.distance = iteration;
+                    inRange.Add(neighbour);
+                    FindNodesInRange(neighbour, range-1, iteration++);
+                }
+            }
+        }
+    }
+}
---
+++ Untitled Strategy Game/Assets/Scripts/Node.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Position position;
+    public int distance;
+
+    public List<Node> neighbours;
+
+    public bool IsPassable()
+    {
+        return !GetComponent<HexGame>().IsOccupied();
+    }
+
+    [System.Serializable]
+    public class Position
+    {
+        public int x;
+        public int y;
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public bool Equals(Position position)
+        {
+            return (x == position.x) && (y == position.y);
+        }
+    }
+}
